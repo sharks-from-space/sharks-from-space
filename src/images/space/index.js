302209e166fe82dc1cca42cdb1dddb1cecce74ae
Monique Grinstein <moniquecalmon@poli.ufrj.br>
2025-10-05 21:51:31 +0100

    docs: update about sharks content

--- src/images/space/index.js
+++ src/images/space/index.js
@@ -3,5 +3,6 @@ import Tag1 from "./tag1.jpeg";
 import Tag2 from "./tag2.jpeg";
 import Remora from "./remora.jpeg";
 import Track from "./shark_track_animation.gif";
+import WideShark from "./aboutsharks.png";
 
-export {SharksSpace , Tag1, Tag2, Remora, Track};
+export {SharksSpace , Tag1, Tag2, Remora, Track, WideShark};
--- src/pages/AboutSharks.js
+++ src/pages/AboutSharks.js
@@ -1,5 +1,10 @@
-import { TermComponent, WebsiteNavbar, Footer, RoadMapLong, TextComponent } from "../components";
-// import * as images from "../images/long-term-vision";
+import {
+	WebsiteNavbar,
+	Footer,
+	ImageComponent,
+	TextComponent,
+} from "../components";
+import * as images from "../images/space";
 
 export function AboutSharks() {
 	return (
@@ -8,32 +13,56 @@ export function AboutSharks() {
 			<div className="PageContent">
 				<h2 className="term-title">About Sharks</h2>
 
-  <TextComponent text={
-  <p>
-    Sharks are more than just apex predators — they are vital regulators of ocean ecosystems, helping maintain healthy populations of prey species and ensuring biodiversity. These ancient creatures have thrived in the world’s oceans for over 400 million years, yet today many shark populations face unprecedented pressures from overfishing, habitat loss, and climate change.
-  </p>
-  } />
-<br/>
+				<TextComponent
+					text={
+						<p>
+							Sharks are more than just apex predators — they are vital
+							regulators of ocean ecosystems, helping maintain healthy
+							populations of prey species and ensuring biodiversity. These
+							ancient creatures have thrived in the world’s oceans for over 400
+							million years, yet today many shark populations face unprecedented
+							pressures from overfishing, habitat loss, and climate change.
+						</p>
+					}
+				/>
+				<br />
+				<ImageComponent
+					src={images.WideShark}
+					alt="Tag concept idea"
+					width="1150px"
+					className="special-image"
+				/>
 
-    <TextComponent text={
-  <>
-    <p>
-      Understanding where sharks are and how they move is critical for protecting both these species and the ecosystems they support. Predicting shark locations guides conservation efforts, informs fisheries management, and helps humans make decisions that reduce conflict and protect key habitats.
-    </p>
-    
-    <p>
-      NASA satellite data offer a unique perspective on the oceans, revealing patterns in sea surface temperature, ocean currents, and phytoplankton blooms — all of which influence where sharks feed and travel. By combining satellite observations with tagging data, researchers can uncover the hidden links between physical ocean features, prey availability, and shark behavior.
-    </p>
-    
-    <p>
-      In this project, we invite you to <strong>become a Jawstronaut</strong> — an explorer of the deep who uses satellite data and ecological insight to uncover the secrets of shark behavior. As a Jawstronaut, you will track foraging hotspots, explore bio-inspired shark tag concepts, and help translate space technology into tools for ocean conservation.
-    </p>
-  </>
-  } />
-</div>
-		<div className="RoadMapLong">
-			{/* <RoadMapLong /> */}
-		</div>
+				<TextComponent
+					text={
+						<>
+							<p>
+								Understanding where sharks are and how they move is critical for
+								protecting the sharks themselves and the ecosystems they
+								support. Predicting shark locations advises conservation
+								efforts, informs fisheries management, and helps humans make
+								decisions that reduce conflict and protect key habitats.
+							</p>
+
+							<p>
+								Satellite observation offers a unique perspective on the oceans,
+								revealing patterns in sea surface temperature, ocean currents,
+								and phytoplankton blooms — all of which influence where sharks
+								feed and travel. By combining satellite and tagging data,
+								researchers can uncover the hidden links between physical ocean
+								features, prey availability, and shark behavior.
+							</p>
+
+							<p>
+								In this project, we invite you to{" "}
+								<strong>become a Jawstronaut</strong>. Track foraging hotspots,
+								explore bio-inspired shark tag concepts, and help translate
+								space technology into tools for ocean conservation!
+							</p>
+						</>
+					}
+				/>
+			</div>
 			<Footer />
 		</div>
 	);
