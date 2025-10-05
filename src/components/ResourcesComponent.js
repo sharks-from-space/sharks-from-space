8c11c43f19ac63080d995f606906860fe6715e5b
Monique Grinstein <moniquecalmon@poli.ufrj.br>
2025-10-05 22:55:32 +0100

    docs: add references

--- src/components/ResourcesComponent.js
+++ src/components/ResourcesComponent.js
@@ -1,99 +1,58 @@
 import "./ResourcesComponent.css";
-import { FaGithub } from "react-icons/fa"; // Add this import at the top
+import { FaGithub } from "react-icons/fa";
 
-export function ResourcesComponent() {
+export function ResourcesComponent({ references = [], showGithub = true }) {
 	return (
 		<div className="resources-container">
-			<h2 className="resources-title">Resources</h2>
+			<h2 className="resources-title">References</h2>
 
-			<h3 className="institution-title">
-				NASA Resources
-			</h3>
-			<div className="resource-item">
-				<a
-					href="https://pace.gsfc.nasa.gov/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Plankton Aerosols Clouds and Ecosystems (PACE) data for phytoplankton abundance and community composition
-				</a>
-			</div>
-			<br />
-						<div className="resource-item">
-				<a
-					href="https://modis.gsfc.nasa.gov/data/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
- MODIS-Aqua has been flying for just over two decades, and provides an excellent time series with which to compare trends.				</a>
-			</div>
-
-						<br />
-						<div className="resource-item">
-				<a
-					href="https://modis.gsfc.nasa.gov/data/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
- MODIS-Aqua has been flying for just over two decades, and provides an excellent time series with which to compare trends.				</a>
-			</div>
-			{/* <div className="resource-item">
-				<a
-					href="https://dl.iafastro.directory/event/IAC-2025/paper/99090/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Paper: Sustainable Lunar Infrastructure Development
-				</a>
-			</div>
-			<br />
-
-			<h3 className="institution-title">
-				International Space University (ISU) - Space Studies Program 2025
-			</h3>
-			<div className="resource-item">
-				<a
-					href="https://isulibrary.isunet.edu/index.php?lvl=notice_display&id=12277"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Report: ISU Library - Sustainable Lunar Spaceports
-				</a>
-			</div> */}
-			{/* <div className="resource-item video-item">
-				<span className="video-title">
-					Sustainable Lunar Spaceports Presentation (Hanyang University ERICA):{" "}
-				</span>
-				<div className="video-wrapper">
-					<iframe
-						width="600"
-						height="315"
-						src="https://www.youtube.com/embed/mHIf1K2vR90?si=tFafg1huDd7wafrp&amp;start=20204"
-						title="YouTube video player"
-						frameborder="0"
-						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-						referrerpolicy="strict-origin-when-cross-origin"
-						allowfullscreen
-					/>
+			{references.length > 0 && (
+				<div className="references-list">
+					{references.map((ref, index) => (
+						<div key={`ref-${index}-${ref.title?.slice(0, 10) || ref.text?.slice(0, 10)}`} className="reference-item">
+							<span className="reference-number">{index + 1}.</span>
+							<div className="reference-content">
+								{ref.url ? (
+									<a
+										href={ref.url}
+										target="_blank"
+										rel="noopener noreferrer"
+										className="reference-link"
+									>
+										{ref.title || ref.text}
+									</a>
+								) : (
+									<span className="reference-text">{ref.title || ref.text}</span>
+								)}
+								{ref.description && (
+									<span className="reference-description"> - {ref.description}</span>
+								)}
+							</div>
+						</div>
+					))}
 				</div>
-			</div> */}
-			<br />
-			
-			<h3 className="institution-title">Website Source Code</h3>
-			<div className="resource-item github-resource">
-				<FaGithub className="github-icon" />
-				<div className="github-details">
-					<span className="github-title">GitHub Repository</span>
-					<a
-						href="https://github.com/sharks-from-space/sharks-from-space"
-						target="_blank"
-						rel="noopener noreferrer"
-						className="github-link"
-					>
-						sharks-from-space
-					</a>
-				</div>
-			</div>
+			)}
+
+			{showGithub && (
+				<>
+					<h3 className="institution-title">Website Source Code</h3>
+					<div className="resource-item github-resource">
+						<FaGithub className="github-icon" />
+						
+						<div className="github-details">
+							<span className="github-title">GitHub Repository</span>
+							<a
+								href="https://github.com/sharks-from-space/sharks-from-space"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="github-link"
+							>
+								sharks-from-space
+							</a>
+						</div>
+					</div>
+				</>
+			)}
 		</div>
 	);
 }
--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -1,11 +1,76 @@
 import { WebsiteNavbar, Footer, ResourcesComponent } from "../components";
 
 export function Resources() {
+    const references = [
+      {
+        title: "Nature",
+        description: "Mechanical underwater adhesive devices for soft substrates",
+        url:"https://www.nature.com/articles/s41586-025-09304-4"
+
+      },
+            {
+        title: "Environmental Biology of Fishes",
+        description: "Observations of skin color aberrations in four shark species off the coast of southern California, USA",
+        url:"https://www.researchgate.net/publication/379130922_Observations_of_skin_color_aberrations_in_four_shark_species_off_the_coast_of_southern_California_USA"
+
+      },
+                  {
+        title: "The University of Western Australia",
+        description: "Are white patches on whale shark skin cause for concern?",
+        url:"https://www.uwa.edu.au/news/article/2024/september/are-white-patches-on-whale-shark-skin-cause-for-concern"
+
+      },
+                        {
+        title: "Journal of Fish Biology",
+        description: "Observations of hypomelanosis in the nurse shark Ginglymostoma cirratum",
+        url:"https://www.researchgate.net/publication/364154860_Observations_of_hypomelanosis_in_the_nurse_shark_Ginglymostoma_cirratum"
+
+      },
+
+                        {
+        title: "Wikipedia",
+        description: "Argos (satellite system)",
+        url:"https://en.wikipedia.org/wiki/Argos_(satellite_system)"
+
+      },
+                        {
+        title: "Sparkfun",
+        description: "ARGOS Satellite Transceiver Shield - ARTIC R2",
+        url:"https://www.sparkfun.com/argos-satellite-transceiver-shield-artic-r2.html"
+
+      },
+        {
+            title: "NASA",
+            description: "Plankton Aerosols Clouds and Ecosystems data for phytoplankton abundance and community composition",
+            url: "https://pace.gsfc.nasa.gov/"
+        },
+        {
+            title: "NASA",
+            description: "Moderate Resolution Imaging Spectroradiometer (MODIS)-Aqua data for phytoplankton abundance",
+            url: "https://modis.gsfc.nasa.gov/data/"
+        },
+        {
+            title: "NASA",
+            description: "Surface Water and Ocean Topography (SWOT)",
+            url: "https://podaac.jpl.nasa.gov/SWOT"
+        },
+        {
+            title: "NASA",
+            description: "Mesoscale eddies release pelagic sharks from thermal constraints to foraging in the ocean twilight zone",
+            url: "https://www.pnas.org/doi/abs/10.1073/pnas.1903067116"
+        },
+        {
+            title: "NASA",
+            description: "Mesoscale eddies influence the movements of mature female white sharks in the Gulf Stream and Sargasso Sea",
+            url: "https://www.nature.com/articles/s41598-018-25565-8"
+        }
+    ];
+
     return (
         <div className="PageContainer">
           <WebsiteNavbar />
           <div className="PageContent">
-            <ResourcesComponent />
+            <ResourcesComponent references={references} />
           </div>
           <Footer />
         </div>
